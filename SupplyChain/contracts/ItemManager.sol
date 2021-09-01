e6c2731b30f26d3c96dea305fdb8a1a87007c58f
sjoerdsommen <sjoerdsommen@gmail.com>
2021-09-01 10:05:36 +0200

    made supplychain working again

--- SupplyChain/contracts/ItemManager.sol
+++ SupplyChain/contracts/ItemManager.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.1;
 
-import "../../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "../client/node_modules/@openzeppelin/contracts/access/Ownable.sol";
 import "./Item.sol";
 
 contract ItemManager is Ownable {
